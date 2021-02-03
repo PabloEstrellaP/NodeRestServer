69766510dbb6268fbe4c75b5499b4eb3bdd4dd64
Pablo <almendaris123@gmail.com>
2021-02-03 15:40:45 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+require('./config/config')
+
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+ 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/usuario', function (req, res) {
+  res.json('get usuario');
+})
+
+app.post('/usuario', (req, res) => {
+    
+    let body = req.body;
+    
+    if (body.nombre === undefined){
+
+        return res.status(400).json({
+            ok : false,
+            msg : "El nombre es necesario" 
+        });
+
+    }else{
+        res.json({
+            persona : body
+        });
+    }
+});
+ 
+app.put('/usuario/:id', (req, res) => {
+    
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando en el puerto ${process.env.PORT}`)
+});
